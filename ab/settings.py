07f8f705d9de895b3ab43b556e1bd810e164e0dd
pc <mahdiarmazraei@gmail.com>
2023-09-03 15:26:13 +0330

    update

--- ab/settings.py
+++ ab/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://127.0.0.1:6380/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
@@ -139,10 +139,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # myproject/settings.py
 
-# Celery settings
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'UTC'
+
--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Create your tests here.from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Contact
+
+class ContactTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('create_contact')
+        self.data = {
+            'name': 'John Doe',
+            'email': 'johndoe@example.com',
+            'message': 'This is a test message.'
+        }
+
+    def test_create_contact(self):
+        response = self.client.post(self.url, self.data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Contact.objects.count(), 1)
+        contact = Contact.objects.first()
+        self.assertEqual(contact.name, self.data['name'])
+        self.assertEqual(contact.email, self.data['email'])
+        self.assertEqual(contact.message, self.data['message'])
+
+    def test_list_contacts(self):
+        response = self.client.get(reverse('list_contacts'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), Contact.objects.count())
